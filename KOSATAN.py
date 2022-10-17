88b4167f3af594b7cd1de5fe26372261e0669d99
Irsyaad_06 <86083982+irsyaad06@users.noreply.github.com>
2022-10-17 14:32:00 +0700

    Add files via upload

--- KOSATAN.py
+++ KOSATAN.py
@@ -1,12 +1,21 @@
+import getpass
+user = "keane"
+psw = "kosatanedan"
+
 print('Selamat Datang di KOSATAN')
 print('Silahkan Login Terlebih dahulu!')
 id = str(input("ID : "))
-print('Masukan Password')
-pw = str(input("Password : "))
-while pw != "kosatanedan":
+while id != user :
+    print("Tidak ada ID yang terdaftar \nSilahkan Coba lagi")
+    id = str(input("ID : "))
+
+if id == user :
+    print('Masukan Password')
+    pw = getpass.getpass("Password : ")
+while pw != psw:
     print("Maaf Password salah, Silahkan masukan lagi")
-    pw = str(input("Password : "))
-if pw == "kosatanedan" :
+    pw = getpass.getpass("Password : ")
+if  pw == psw :
 
      while True:
          # print("")
@@ -15,18 +24,21 @@ if pw == "kosatanedan" :
          if menu == 1 :
 
              print("\nSelamat Datang di Backroom KOSATAN \nKami menyediakan lonte yang sangat enak, seenak fitbar")
-             listlon = ["XX-Simar", "XX-Rajah"]
-             pilon  = int(input("pilih mau lonte yang mana? \n 1.Simar \n 2.Raja \nMasukan Pilihan anda (ketik angka) : "))
-             while True :
-                if pilon == 1 :
-                  print("Kamu memilih onta yang bernama : ", listlon[0])
-                  break
-                elif pilon == 2 :
-                  print("Kamu memilih onta yang bernama : ", listlon[1])
-                  break
-                else :
-                 print("GOBLOK DISURUH 1/2 MALAH MILIH YANG LAIN MAMPUS LOOPING")
-             break
+             list_ont = ["asd","asd"]
+             for i in list_ont:
+                print(i)
+
+                # pilon  = int(input("\nMasukan Pilihan anda (ketik angka) : "))
+                # while True :
+                #     if pilon == 1 :
+                #      print("Kamu memilih onta yang bernama : ", list_onta[0])
+                #      break
+                #     elif pilon == 2 :
+                #      print("Kamu memilih onta yang bernama : ", list_onta[1])
+                #      break
+                #     else :
+                #      print("GOBLOK DISURUH 1/2 MALAH MILIH YANG LAIN MAMPUS LOOPING")
+                # break
 
          elif menu == 2 :
              print("Selamat Datang di Casino KOSATAN \nAnda menang kami tidak senang")
@@ -37,9 +49,9 @@ if pw == "kosatanedan" :
                  salah = []
                  print("Kamu Memilih Quiz")
                  print("1.Jika Fadlol mengeluarkan kartu Flush\nAgar Menang kamu mengeluarkan kartu... ")
-                 jaku = int(input("A.Pair of King\nB.Straight\nC.Full House\nD.Pair of 2\nJawaban :"))
+                 jaku = str(input("A.Pair of King\nB.Straight\nC.Full House\nD.Pair of 2\nJawaban :"))
                  print("2.Jika Adlos mengeluarkan kartu Fullhouse of Queen\nAgar Menang kamu mengeluarkan kartu... ")
-                 jaku = int(input("A.Straight\nB.Flush\nC.Royal Flush\nD.Pair of 2\nJawaban :"))
+                 jaku = str(input("A.Straight\nB.Flush\nC.Royal Flush\nD.Pair of 2\nJawaban :"))
 
                  benar.append(jaku)
 
